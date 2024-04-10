c2bbdc7354c9cecef1789ae9902171d0907bcbea
Ausgore <glenn.marcell.tjandra@gmail.com>
2024-04-10 18:44:07 +0800

    Updated client class

--- lib/classes/client.d.ts
+++ lib/classes/client.d.ts
@@ -1,9 +1,10 @@
-import { Client as DiscordClient, ClientOptions } from "discord.js";
+import { Client as DiscordClient, ClientOptions, Collection } from "discord.js";
 import { ClientProps } from "../typings";
 import Command from "./command";
 export default class Client extends DiscordClient implements ClientProps {
-    commands: Map<string, Command>;
+    commands: Collection<string, Command>;
     constructor(options: ClientOptions);
     init(token: string): Promise<void>;
     private register;
+    loadSlashCommands(): Promise<void>;
 }
--- src/classes/client.ts
+++ src/classes/client.ts
@@ -1,4 +1,4 @@
-import { Client as DiscordClient, ClientOptions } from "discord.js";
+import { Client as DiscordClient, ClientOptions, REST, Routes, Collection } from "discord.js";
 import { ClientProps } from "../typings";
 import Command from "./command";
 import { promises as fs } from "fs";
@@ -6,7 +6,7 @@ import path from "path";
 import Event from "./event";
 
 export default class Client extends DiscordClient implements ClientProps {
-	public commands: Map<string, Command> = new Map();
+	public commands: Collection<string, Command> = new Collection();
 	constructor(options: ClientOptions) {
 		super(options);
 	}
@@ -15,6 +15,7 @@ export default class Client extends DiscordClient implements ClientProps {
 		await this.register(`${path.dirname(require.main.filename)}/commands`);
 		await this.register(`${path.dirname(require.main.filename)}/events`);
 		this.once("ready", async (client) => {
+			if (this.commands.size) await this.loadSlashCommands();
 			console.log(`Successfully logged in as \u001b[32m${client.user.tag}\u001b[0m!`);
 		});
 		this.login(token);
@@ -22,7 +23,7 @@ export default class Client extends DiscordClient implements ClientProps {
 
 	private async register(dir: string) {
 		const files = await fs.readdir(dir).catch(() => null);
-		if (!files) return console.log(`\x1b[31m- ${dir.split("/")[1]} folder cannot be found.\x1b[37m`);
+		if (!files?.length) return console.log(`\x1b[31m - ${dir.split("/")[1].slice(0, 1).toUpperCase() + dir.split("/")[1].slice(1, -1)} folder cannot be found\x1b[37m`);
 
 		await Promise.all(files.map(async (file) => {
 			const filePath = path.join(dir, file);
@@ -35,5 +36,13 @@ export default class Client extends DiscordClient implements ClientProps {
 				else if (data instanceof Event) this.on(data.event, data.on.bind(null, this));
 			}
 		}));
+	}
+
+	public async loadSlashCommands() {
+		console.log(" - Clearing existing \u001b[34;1mapplication (/) commands\u001b[0m");
+		const rest = new REST().setToken(this.token);
+		const commands = this.commands.map(c => c.data);
+		await rest.put(Routes.applicationCommands(this.user.id), { body: commands });
+		console.log(" - Successfully reloaded \u001b[34;1mapplication (/) commands\u001b[0m");
 	}
 }
