845a21a2790be042c1a2b5fda225356407bbe6ab
Ausgore <glenn.marcell.tjandra@gmail.com>
2024-05-18 02:13:37 +0800

    SlashCommandBuilder typing on data set to any
    I can't be bothered to figure a way to account for all types of SlashCommandBuilders.

--- lib/classes/command.d.ts
+++ lib/classes/command.d.ts
@@ -1,7 +1,7 @@
-import { AutocompleteInteraction, ChatInputCommandInteraction, SlashCommandBuilder, SlashCommandSubcommandsOnlyBuilder } from "discord.js";
+import { AutocompleteInteraction, ChatInputCommandInteraction } from "discord.js";
 import { CommandProps } from "../typings";
 export default class Command implements CommandProps {
-    data: SlashCommandBuilder | Omit<SlashCommandBuilder, "addSubcommand" | "addSubcommandGroup"> | SlashCommandSubcommandsOnlyBuilder;
+    data: any;
     run: (interaction: ChatInputCommandInteraction) => void;
     autocomplete?: (interaction: AutocompleteInteraction) => void;
     constructor(props: CommandProps);
--- src/classes/command.ts
+++ src/classes/command.ts
@@ -1,8 +1,8 @@
-import { AutocompleteInteraction, ChatInputCommandInteraction, SlashCommandBuilder, SlashCommandSubcommandsOnlyBuilder } from "discord.js";
+import { AutocompleteInteraction, ChatInputCommandInteraction } from "discord.js";
 import { CommandProps } from "../typings";
 
 export default class Command implements CommandProps {
-	public data: SlashCommandBuilder | Omit<SlashCommandBuilder, "addSubcommand" | "addSubcommandGroup"> | SlashCommandSubcommandsOnlyBuilder;
+	public data: any;
 	public run: (interaction: ChatInputCommandInteraction) => void;
 	public autocomplete?: (interaction: AutocompleteInteraction) => void;
 	constructor(props: CommandProps) {
