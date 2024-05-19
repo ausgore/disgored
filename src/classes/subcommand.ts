bb5d5e99388135136e283cf7e0e7a364c4fb2cec
Ausgore <glenn.marcell.tjandra@gmail.com>
2024-05-19 13:17:43 +0800

    Allow subcommandGroup to allow undefined

--- src/classes/subcommand.ts
+++ src/classes/subcommand.ts
@@ -28,7 +28,7 @@ const loadSubcommands = async function(dir: string, collection: Collection<strin
 	}
 }
 
-const getSubcommand = function(subcommands: Collection<string, Subcommand>, subcommand: string, subcommandGroup?: string) {
+const getSubcommand = function(subcommands: Collection<string, Subcommand>, subcommand: string, subcommandGroup?: string | undefined) {
 	return subcommands.get(subcommandGroup ? `${subcommand}-${subcommandGroup}` : subcommand);
 }
 
