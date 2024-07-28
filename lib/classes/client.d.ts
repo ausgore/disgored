c6392663e5c31278ab8934226fcb4f50a4f4498b
Ausgore <glenn.marcell.tjandra@gmail.com>
2024-07-28 14:25:42 +0800

    Minor tweaks

--- lib/classes/client.d.ts
+++ lib/classes/client.d.ts
@@ -2,7 +2,7 @@ import { Client as DiscordClient, ClientOptions, Collection } from "discord.js";
 import { ClientProps, InitOptionsProps } from "../typings";
 import Command from "./command";
 import Subcommand from "./subcommand";
-export default class Client extends DiscordClient implements ClientProps {
+export default class Client<Ready extends boolean = boolean> extends DiscordClient<Ready> implements ClientProps {
     commands: Collection<string, Command>;
     subcommands: Collection<string, Subcommand>;
     constructor(options: ClientOptions);
--- src/classes/event.ts
+++ src/classes/event.ts
@@ -1,6 +1,5 @@
 import { ClientEvents } from "discord.js";
 import { EventProps } from "../typings";
-import Client from "./client";
 
 export default class Event<T extends string | keyof ClientEvents = keyof ClientEvents> implements EventProps<T> {
 	public event: T;
